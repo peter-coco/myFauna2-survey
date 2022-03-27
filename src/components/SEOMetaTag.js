f1307878460b19bfcebd825223278304425c4f48
peter-coco <dklirser@gmail.com>
2022-03-27 16:50:21 +0900

    retest on seoMeta.js

--- src/components/SEOMetaTag.js
+++ src/components/SEOMetaTag.js
@@ -9,16 +9,16 @@ const MetaTag = (props) => {
       <meta name="description" content={props.description} />
       <meta name="keywords" content={props.keywords} />
 
-      <meta property="og:type" content="website" />
       <meta property="og:title" content={props.title} />
-      <meta property="og:site_name" content={props.title} />
       <meta property="og:description" content={props.description} />
       <meta property="og:image" content={props.imgsrc} />
       <meta property="og:url" content={props.url} />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
+      {/* <meta property="og:site_name" content={props.title} /> */}
+      {/* <meta property="og:type" content="website" /> */}
+      {/* <meta property="og:image:width" content="1200" /> */}
+      {/* <meta property="og:image:height" content="630" /> */}
 
-      <link rel="canonical" href={props.url} />
+      {/* <link rel="canonical" href={props.url} /> */}
     </Helmet>
   );
 };
